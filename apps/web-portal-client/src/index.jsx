8b92fce0be93a763328c496b868ed26f46ab10d0
Alex <Jurchenko.a@gmail.com>
2020-11-22 16:18:54 +0200

    gh-23 Add LoginPage component - Delete old static pages - Add LoginPage component - Include LoginPage component into index

--- apps/web-portal-client/src/index.jsx
+++ apps/web-portal-client/src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {HashRouter} from 'react-router-dom'
+import LoginPage from "./LoginPage.jsx"
+import { HashRouter } from 'react-router-dom'
 // import {AppContainer} from './app/AppContainer'
 import './index.css'
 
 // TODO: gh-23 Add Login page as component here
 ReactDOM.render(
-  <div>Hello</div>,
+  <LoginPage />,
   document.getElementById('app')
 )
