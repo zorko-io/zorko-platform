a05c70f3c4677f0247963f9f5a19e4e6650d8c61
nesterone <igornester@gmail.com>
2020-11-28 23:09:54 +0200

    gh-39 Change ui-validation package API - extend and fix tests

--- apps/web-portal-client/lib/common/Validator.jsx
+++ apps/web-portal-client/lib/common/Validator.jsx
@@ -8,21 +8,20 @@ export default function Validator() {
     gender: {one_of: ['male', 'female']},
     phone: {max_length: 10},
     password: ['required', {min_length: 10}],
-    password2: {equal_to_field: 'password'},
+    password2: {equal_to_field: 'password'}
   }
-  const params = {
+  const value = {
     name: 'Name',
     email: 'test@gmail.com',
     gender: 'male',
     phone: '+123456789',
     password: 'password12',
-    password2: 'password12',
+    password2: 'password12'
   }
 
-  try {
-    const result = createValidator(rules).validate(params)
-    return <div>{JSON.stringify(result)}</div>
-  } catch (err) {
-    return <div>Validation Error</div>
-  }
+  const {error, result} = createValidator(rules).parse(value)
+
+  return !error ?
+    <div>{JSON.stringify(result)}</div> :
+    <div>{error.message}</div>
 }
