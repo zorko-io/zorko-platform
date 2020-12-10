6062c647708f6b81134fa9e442a5ff65961cfb2a
Alex <Jurchenko.a@gmail.com>
2020-12-10 17:38:44 +0200

    gh-67 Extract class for MobileMenuButton

--- apps/web-portal-client/lib/components/nav-bar/MobileMenuButton.jsx
+++ apps/web-portal-client/lib/components/nav-bar/MobileMenuButton.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './mobileMenuButton.css'
 
 export function MobileMenuButton(props) {
   const {shape, onToggle} = props
 
   return (
     <div className="-mr-2 flex md:hidden">
-      <button
-        type="submit"
-        onClick={onToggle}
-        className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-      >
+      <button type="submit" onClick={onToggle} className="btn-menu">
         <span className="sr-only">Open main menu</span>
 
         <svg
