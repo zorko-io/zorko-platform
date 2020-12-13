434bb2201456bdccdfcd0bbc442cea04b2ca3dbc
Alex <Jurchenko.a@gmail.com>
2020-12-13 13:12:29 +0200

    fix issues with path

--- apps/web-portal-client/lib/features/examples/ExamplesPage.jsx
+++ apps/web-portal-client/lib/features/examples/ExamplesPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import {Layout} from '../components/layout/Layout'
-import {HeaderLayout} from '../components/layout/HeaderLayout'
-import {SidebarLayout} from '../components/layout/SidebarLayout'
-import {ContentLayout} from '../components/layout/ContentLayout'
+import {Layout} from './components/layout/Layout'
+import {HeaderLayout} from './components/layout/HeaderLayout'
+import {SidebarLayout} from './components/layout/SidebarLayout'
+import {ContentLayout} from './components/layout/ContentLayout'
 
 export function ExamplesPage() {
   return (
--- apps/web-portal-client/lib/features/examples/components/layout/ContentLayout.jsx
+++ apps/web-portal-client/lib/features/examples/components/layout/ContentLayout.jsx
@@ -1,12 +1,5 @@
 import React from 'react'
-import ValidatorExample from '../../common/ValidatorExample'
-import ApiExample from '../../common/ApiExample'
 
 export function ContentLayout() {
-  return (
-    <div>
-      <ValidatorExample />
-      <ApiExample />
-    </div>
-  )
+  return <div>Content</div>
 }
