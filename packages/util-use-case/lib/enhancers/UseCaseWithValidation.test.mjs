602a09e0836ccf18e21c4af4404ed9900a85351f
nesterone <igornester@gmail.com>
2020-12-17 23:01:21 +0200

    gh-55 Prepare util-use-case - more tests

--- packages/util-use-case/lib/enhancers/UseCaseWithValidation.test.mjs
+++ packages/util-use-case/lib/enhancers/UseCaseWithValidation.test.mjs
@@ -1,15 +1,27 @@
 import test from '@zorko-io/tool-test-harness'
 import {UseCaseWithValidation} from './UseCaseWithValidation.mjs'
 import {UseCase} from '../core'
+import sinon from 'sinon'
 
 test('create UseCase with default context', async (t) => {
 
   const createValidator = () => {}
 
+  const origin = sinon.createStubInstance(UseCase)
+  const params = {}
+  const result = {}
+
+  origin.run.returns(Promise.resolve(result))
+
   const useCase = new UseCaseWithValidation({
-    origin: new UseCase(),
+    origin,
     createValidator
   })
 
-  t.assert(useCase)
+  const actual = await useCase.run(params)
+
+  t.is(actual, result)
+
+  t.assert(origin.run.calledOnce)
+  t.deepEqual(origin.run.firstCall.args[0], params)
 })
