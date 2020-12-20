6b4ac75dde6c8b28adc84ae529f71f1735024a22
Alex <Jurchenko.a@gmail.com>
2020-12-20 16:05:38 +0200

    gh-65 Move all examples to examples/components

---
+++ apps/web-portal-client/lib/features/examples/components/ExamplesPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Content, Header, Sidebar, Navbar, Layout} from '../../../components/layout'
+export function ExamplesPage() {
+  return (
+    <Layout
+      navbarRender={() => <Navbar />}
+      sidebarRender={() => (
+        <Sidebar
+          description="Get started"
+          items={[
+            {name: 'Buttons', link: '#', active: true},
+            {name: 'Forms', link: '#'},
+          ]}
+        />
+      )}
+      headerRender={() => <Header title="Our Examples" />}
+      contentRender={() => <Content innerContentRender={() => <div>Examples</div>} />}
+    />
+  )
+}
+
+ExamplesPage.propTypes = {}
+
+ExamplesPage.defaultProps = {}
---
+++ apps/web-portal-client/lib/features/examples/components/ValidatorExample.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {createValidator} from '@util-validation'
+
+export default function ValidatorExample() {
+  const rules = {
+    name: 'required',
+    email: ['required', 'email'],
+    gender: {one_of: ['male', 'female']},
+    phone: {max_length: 10},
+    password: ['required', {min_length: 10}],
+    password2: {equal_to_field: 'password'},
+  }
+  const value = {
+    name: 'Name',
+    email: 'test@gmail.com',
+    gender: 'male',
+    phone: '+123456789',
+    password: 'password12',
+    password2: 'password12',
+  }
+
+  const validator = createValidator(rules)
+  const {error, result} = validator.parseSync(value)
+
+  return !error ? <div>{JSON.stringify(result)}</div> : <div>{error.message}</div>
+}
