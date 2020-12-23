65ce4bbcdb3addac75821fa34e08d5377db88570
nesterone <igornester@gmail.com>
2020-12-23 19:28:20 +0200

    gh-55 Prepare util-use-case - prep test context

--- packages/util-use-case/lib/helpers/makeRunner.test.mjs
+++ packages/util-use-case/lib/helpers/makeRunner.test.mjs
@@ -3,15 +3,6 @@ import {UseCase} from '../core'
 import {makeRunner} from './makeRunner'
 import sinon from 'sinon'
 
-
-class MockUseCase extends UseCase {
-
-  async run (params) {
-    return {params}
-  }
-
-}
-
 class UseCaseWithStub extends UseCase {
 
   static stubRun = sinon.stub()
@@ -21,42 +12,50 @@ class UseCaseWithStub extends UseCase {
   }
 }
 
+test.beforeEach((t) => {
+  const params = { limit: '10'}
+  const result = { data: { result: 'aaaa'}}
+  const req = {
+    query: params
+  }
+  const res = {
+    send : () => {}
+  }
+
+  let run = sinon.stub().withArgs(params).returns(Promise.resolve(result))
+  UseCaseWithStub.stubRun = run
+
+  t.context = {
+    run, result, params, req, res
+  }
+})
+
+
 // TODO: gh-55 cover with unit tests
 test.serial('run with defaults', async (t) => {
-  let run = sinon.stub()
-  UseCaseWithStub.stubRun = run
+  const {run, result, req, res} = t.context
+
 
   const runner = makeRunner(UseCaseWithStub)
 
-  await runner()
+  const actual = await runner(req, res)
 
   t.assert(run.calledOnce)
   t.deepEqual(run.firstCall.args[0], {})
+  t.deepEqual(actual, result)
 })
 
 test.serial('run with  params', async (t) => {
-  const query = { limit: '10'}
-  const result = { data: { result: 'aaaa'}}
-
-  let run = sinon.stub().returns(Promise.resolve(result))
-  UseCaseWithStub.stubRun = run
+  const {run, result, res, req, params} = t.context
 
   const runner = makeRunner(UseCaseWithStub, {
     toParams: ({query}) => ({...query})
   })
 
-  const req = {
-    query
-  }
-
-  const res = {
-    send : () => {}
-  }
-
   const actual = await runner(req, res)
 
   t.assert(run.calledOnce)
-  t.deepEqual(run.firstCall.args[0], query)
+  t.deepEqual(run.firstCall.args[0], params)
   t.deepEqual(actual, result)
 
 })
