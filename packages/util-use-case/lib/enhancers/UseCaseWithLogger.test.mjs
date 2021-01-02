3316bec6020e2e096a871944c4847bd890204667
yzhbankov <yzhbankov@gmail.com>
2021-01-02 12:35:27 +0200

    gh-121. cover runtime tracking with the unit tests

--- packages/util-use-case/lib/enhancers/UseCaseWithLogger.test.mjs
+++ packages/util-use-case/lib/enhancers/UseCaseWithLogger.test.mjs
@@ -1,10 +1,10 @@
 import test from '@zorko-io/tool-test-harness'
 import sinon from 'sinon'
+import {CoreLogger} from '@zorko-io/util-logger'
 import {UseCaseWithLogger} from './UseCaseWithLogger'
 import {UseCase} from '../core'
-import {CoreLogger} from '@zorko-io/util-logger'
 
-test('create UseCase with custom logger', async (t) => {
+test('create UseCase with custom logger and runtime tracking', async (t) => {
   const log = sinon.createStubInstance(CoreLogger)
   const origin = sinon.createStubInstance(UseCase)
   const params = {name: 'bbb'}
@@ -18,7 +18,7 @@ test('create UseCase with custom logger', async (t) => {
   const useCase = new UseCaseWithLogger({
     origin,
     log,
-    name
+    name,
   })
 
   const actual = await useCase.run(params)
@@ -26,11 +26,52 @@ test('create UseCase with custom logger', async (t) => {
   t.is(actual, result)
 
   t.assert(origin.run.calledOnce)
+  t.assert(log.debug.calledOnce)
 
   t.deepEqual(log.trace.firstCall.args[0], 'Start use case execution')
+
+  const debugPayload = log.debug.firstCall.args[0]
+  t.deepEqual(debugPayload.useCase, name)
+  t.deepEqual(debugPayload.params, params)
+  t.deepEqual(debugPayload.result, result)
+  t.assert(debugPayload.runtime > 0)
+
   t.assert(log.trace.calledBefore(origin.run))
 
   t.deepEqual(log.info.firstCall.args[0], 'Finish use case execution')
   t.assert(log.info.calledAfter(origin.run))
+})
 
+test('create UseCase with custom logger and runtime tracking. Error case', async (t) => {
+  const log = sinon.createStubInstance(CoreLogger)
+  const origin = sinon.createStubInstance(UseCase)
+  const params = {name: 'bbb'}
+  const name = 'TestUseCase'
+  const error = {code: 'CUSTOM_ERROR', message: 'Error message', stack: 'test stacktrace'}
+
+  log.child.returns(log)
+
+  origin.run.withArgs(params).returns(Promise.reject(error))
+
+  const useCase = new UseCaseWithLogger({
+    origin,
+    log,
+    name,
+  })
+
+  await useCase.run(params).catch((err) => {
+    t.assert(err)
+  })
+
+  t.assert(origin.run.calledOnce)
+  t.assert(log.debug.calledOnce)
+  t.deepEqual(log.error.firstCall.args[0], 'Issues with running use case')
+
+  const debugPayload = log.debug.firstCall.args[0]
+  t.deepEqual(debugPayload.useCase, name)
+  t.deepEqual(debugPayload.params, params)
+  t.deepEqual(debugPayload.error, error)
+  t.assert(debugPayload.runtime > 0)
+
+  t.assert(log.trace.calledBefore(origin.run))
 })
