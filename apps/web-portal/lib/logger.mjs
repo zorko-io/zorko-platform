7c4c78cc7231c34c25baa2f077ff0400a6f6b223
yzhbankov <yzhbankov@gmail.com>
2021-01-03 21:32:39 +0200

    gh-112. cleanup

--- apps/web-portal/lib/logger.mjs
+++ apps/web-portal/lib/logger.mjs
@@ -1,28 +1,28 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-unused-expressions */
-let _LOGGER
+let LOGGER
 
 export function setLogger(logger) {
-  _LOGGER = logger
+  LOGGER = logger
 }
 
 export default {
   fatal(data) {
-    _LOGGER ? _LOGGER.fatal(data) : console.debug(data)
+    LOGGER ? LOGGER.fatal(data) : console.debug(data)
   },
   error(data) {
-    _LOGGER ? _LOGGER.error(data) : console.error(data)
+    LOGGER ? LOGGER.error(data) : console.error(data)
   },
   warn(data) {
-    _LOGGER ? _LOGGER.warn(data) : console.warn(data)
+    LOGGER ? LOGGER.warn(data) : console.warn(data)
   },
   info(data) {
-    _LOGGER ? _LOGGER.info(data) : console.info(data)
+    LOGGER ? LOGGER.info(data) : console.info(data)
   },
   debug(data) {
-    _LOGGER ? _LOGGER.debug(data) : console.debug(data)
+    LOGGER ? LOGGER.debug(data) : console.debug(data)
   },
   trace(data) {
-    _LOGGER ? _LOGGER.trace(data) : console.debug(data)
+    LOGGER ? LOGGER.trace(data) : console.debug(data)
   },
 }
