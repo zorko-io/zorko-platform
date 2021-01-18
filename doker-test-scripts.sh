fa18203d4cdc14313849879713f11ebd33413cd4
nesterone <igornester@gmail.com>
2021-01-18 21:53:15 +0200

    gh-16 Docker infra for devs - fix merge issues

--- doker-test-scripts.sh
+++
@@ -1,8 +0,0 @@
-# Temp solution to check containers
-
-# Build backend with context
-docker build -t backend-1 -f dev-ops/backend/Dockerfile .
-
-# TODO: run with custom ports, volumes and commands
-
-# TODO: build and run with nginx
