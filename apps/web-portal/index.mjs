ed617c054090c1081265a41844e2fdf58c7859b8
yzhbankov <yzhbankov@redpointpositioning.com>
2021-02-06 11:14:44 +0000

    gh-184. Cleanup

--- apps/web-portal/index.mjs
+++ apps/web-portal/index.mjs
@@ -1,13 +1,11 @@
-import {createLogger, LoggerTypes} from '@zorko-io/util-logger'
+import {createLogger} from '@zorko-io/util-logger'
 import {WebPortalExpressApp} from './lib'
 import {config} from './config'
 
 const logger = createLogger({})
-const expressLogger = createLogger({type: LoggerTypes.PinoExpress})
 
 const app = new WebPortalExpressApp({
   logger,
-  expressLogger,
   process,
   config,
 })
