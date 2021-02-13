802e0e0e73579a0c33b502afba1796f69494beb8
WhiteAdam <jurchenko.a@gmail.com>
2021-02-13 18:45:12 +0200

    gh-185 Integrate web-api-client with web-portal-client over Axios - change mock client to axios

--- apps/web-portal-client/lib/api/index.jsx
+++ apps/web-portal-client/lib/api/index.jsx
@@ -1,3 +1,6 @@
-import {createClient} from '@util-web-api-client'
+import {createClient, ClientTypes} from '@util-web-api-client'
 
-export const client = createClient()
+export const client = createClient({
+  type: ClientTypes.Axios,
+  options: {baseURL: 'http://localhost:7777/'},
+})
