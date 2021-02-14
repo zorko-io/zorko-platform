fe1d21995335657eb91666c94f92c33c34cc100d
yzhbankov <yzhbankov@redpointpositioning.com>
2021-02-14 17:11:08 +0000

    gh-185. Run web portal server to run e2e tests with axios client

--- apps/web-portal/lib/rest-api-v1/index.mjs
+++ apps/web-portal/lib/rest-api-v1/index.mjs
@@ -11,6 +11,7 @@ export function route(deps) {
   const previewController = preview(deps)
   const authController = auth(deps)
 
+  router.get('/', (req, res) => { res.status(200).send({}) })
   router.get('/previews', previewController.list)
   router.post('/auth/login', authController.login)
 
