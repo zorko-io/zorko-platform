9dfe0433573e62ef5857f4c3f0a4824c328ae81b
WhiteAdam <jurchenko.a@gmail.com>
2021-02-18 09:38:18 +0200

    gh-185 Integrate web-api-client with web-portal-client over Axios - add TODO mark

--- apps/web-portal-client/lib/api/index.jsx
+++ apps/web-portal-client/lib/api/index.jsx
@@ -1,5 +1,6 @@
 import {createClient, ClientTypes} from '@util-web-api-client'
 
+// TODO: Move base url value to app config/envs
 export const client = createClient({
   type: ClientTypes.Axios,
   options: {baseURL: 'http://localhost:7777/'},
