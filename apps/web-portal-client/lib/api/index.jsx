3e9715548253e47a03ac71faee75bfff9ea06499
yzhbankov <yzhbankov@gmail.com>
2021-02-25 09:29:16 +0200

    gh-207. Fix discussions

--- apps/web-portal-client/lib/api/index.jsx
+++ apps/web-portal-client/lib/api/index.jsx
@@ -2,5 +2,5 @@ import {ClientTypes, createClient} from '@util-web-api-client'
 
 export const client = createClient({
   type: ClientTypes.Axios,
-  options: {baseURL: process.env.API_BASE_URL},
+  options: {baseURL: process.env.ZORKO_API_BASE_URL},
 })
