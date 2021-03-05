5d219fef7c75c177b8c38b25ee0644a36e012bb0
nesterone <igornester@gmail.com>
2021-03-05 22:42:13 +0200

    gh-228 Common approach to locate and define configuration - update tests

--- packages/util-config/lib/createConfigDiscovery.test.mjs
+++ packages/util-config/lib/createConfigDiscovery.test.mjs
@@ -5,9 +5,7 @@ import validation from '../config-schema.json'
 import path from 'path'
 import someOtherStructure from '../someconfigs/config.json'
 import someOtherValidation from '../someconfigs/config-schema.json'
-
 import {ConfmeConfigDiscovery} from './confme/index.mjs'
-import sinon from 'sinon'
 
 test('async - parses valid value', async (t) => {
   const discovery = createConfigDiscovery()
@@ -25,25 +23,4 @@ test('async - parses with specific folder', async (t) => {
   t.true(discovery instanceof ConfmeConfigDiscovery)
   t.deepEqual(discovery.schema.structure, someOtherStructure)
   t.deepEqual(discovery.schema.validation,someOtherValidation )
-})
-
-
-test('calls confme for confs discovery', async (t) => {
-  // TODO: gh-228 move to other file
-
-  const definition =  'foo/foo'
-  const validation = 'boo/boo'
-  const expected = {key1: 'someresult'}
-  const confme = sinon.stub().returns(expected)
-
-  const discovery = new ConfmeConfigDiscovery({
-    definition,
-    validation
-  }, {confme})
-
-  const actual = discovery.discover()
-
-  t.true(confme.calledOnce, 'should call confme')
-  t.deepEqual(confme.firstCall.args, [definition, validation])
-  t.deepEqual(actual, expected)
 })
