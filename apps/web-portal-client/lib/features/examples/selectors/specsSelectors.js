3fbc5f7a78475993c39a081d659655efc47b267c
yzhbankov <yzhbankov@redpointpositioning.com>
2021-03-10 20:23:33 +0000

    gh-186. Remove specs hook, slices, reducers

--- apps/web-portal-client/lib/features/examples/selectors/specsSelectors.js
+++
@@ -1,3 +0,0 @@
-export const selectSpecs = (state) => state.examples.specs.list
-export const selectLoadingSpecs = (state) => state.examples.specs.loading
-export const selectSpecsError = (state) => state.examples.specs.error
--- apps/web-portal-client/lib/features/examples/slices/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from '@reduxjs/toolkit'
-import specsReducer from './specsSlice'
-
-export default combineReducers({
-  specs: specsReducer,
-})
