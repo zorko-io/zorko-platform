fbaebcd98ef38eaedae0bdd7bb68c04a63c4593b
yzhbankov <yzhbankov@redpointpositioning.com>
2021-03-10 20:54:28 +0000

    gh-186. Remove specs hook, slices, reducers

--- apps/web-portal-client/lib/store/index.js
+++ apps/web-portal-client/lib/store/index.js
@@ -1,7 +1,6 @@
 import {configureStore} from '@reduxjs/toolkit'
 import {logger} from '../logger'
 import authReducer from '../features/auth/slices'
-import examplesReducer from '../features/examples/slices'
 
 const loggerMiddleware = () => (next) => (action) => {
   logger.info(action)
@@ -11,7 +10,6 @@ const loggerMiddleware = () => (next) => (action) => {
 export default configureStore({
   reducer: {
     auth: authReducer,
-    examples: examplesReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(loggerMiddleware),
 })
