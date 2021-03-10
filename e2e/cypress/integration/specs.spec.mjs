991daeb29bf0ceb7c14bf9405ed23df80c991fe5
yzhbankov <yzhbankov@redpointpositioning.com>
2021-03-10 07:37:42 +0000

    gh-186. Fix discussions. Remove e2e tests for Alex future implementation

--- e2e/cypress/integration/specs.spec.mjs
+++ e2e/cypress/integration/specs.spec.mjs
@@ -7,13 +7,6 @@ describe('Render Vega-Lite Specs', function () {
   })
 
   it('render vega-lite examples based on specs', function () {
-    cy.viewport(1024, 768)
-    cy.get('[data-test-id="email"]').type(email)
-    cy.get('[data-test-id="password"]').type(password)
-    cy.get('[data-test-id="submit"]').click()
-    cy.get('[data-test-id="example"]').click()
-    cy.get('[data-test-id="vegaLiteExampleBtn"]').click()
-
-    cy.get('[data-test-id="vegaLiteItem"]').should('have.length', 3)
+    // TODO: implement e2e integration test for specs rendering
   })
 })
