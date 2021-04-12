7039490edcaf18076de0c4d36d6490872763a92c
nesterone <igornester@gmail.com>
2021-04-12 19:23:02 +0300

    gh-242 Provide spaces access module - prep content test for more test cases

--- packages/access-content/lib/mongo/content/MongoContentAccess.test.mjs
+++ packages/access-content/lib/mongo/content/MongoContentAccess.test.mjs
@@ -15,48 +15,58 @@ setupDb(test, async (t) => {
   }
 })
 
-test.serial('add new record', async (t) => {
-  const {content} = t.context
-
-  const spec =  {
-      $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-      description: 'A simple bar chart with embedded data.',
-      data: {
-        values: [
-          {a: 'A', b: 28},
-          {a: 'B', b: 55},
-          {a: 'C', b: 43},
-          {a: 'D', b: 91},
-          {a: 'E', b: 81},
-          {a: 'F', b: 53},
-          {a: 'G', b: 19},
-          {a: 'H', b: 87},
-          {a: 'I', b: 52},
-        ],
-      },
-      mark: 'bar',
-      encoding: {
-        x: {field: 'a', type: 'nominal', axis: {labelAngle: 0}},
-        y: {field: 'b', type: 'quantitative'},
-      },
+test.beforeEach((t)=> {
+  const barCharSpec =  {
+    $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+    description: 'A simple bar chart with embedded data.',
+    data: {
+      values: [
+        {a: 'A', b: 28},
+        {a: 'B', b: 55},
+        {a: 'C', b: 43},
+        {a: 'D', b: 91},
+        {a: 'E', b: 81},
+        {a: 'F', b: 53},
+        {a: 'G', b: 19},
+        {a: 'H', b: 87},
+        {a: 'I', b: 52},
+      ],
+    },
+    mark: 'bar',
+    encoding: {
+      x: {field: 'a', type: 'nominal', axis: {labelAngle: 0}},
+      y: {field: 'b', type: 'quantitative'},
+    },
   }
 
   const mime = MimeTypes.VegaLite
 
+  const defaultJoeRepo = {
+    name: 'default',
+    owner: 'joe'
+  }
+
+  const contentWithBarChart = {
+    content: { spec :barCharSpec},
+    mime
+  }
+
+  t.context.barCharSpec = barCharSpec
+  t.context.contentWithBarChart = contentWithBarChart
+  t.context.defaultJoeRepo = defaultJoeRepo
+})
+
+test.serial('add new record', async (t) => {
+  const {content, contentWithBarChart , defaultJoeRepo, barCharSpec} = t.context
+
   const result = await content.add({
-    content: {
-      mime,
-      content: {spec},
-    },
-    repository: {
-      name: 'default',
-      owner: 'joe'
-    }
+    content: contentWithBarChart,
+    repository: defaultJoeRepo
   })
 
   t.truthy(result)
 
-  t.deepEqual(result.content, {spec})
-  t.is(result.mime, mime)
+  t.deepEqual(result.content, {spec: barCharSpec})
+  t.is(result.mime, contentWithBarChart.mime)
   t.truthy(typeof result.id === 'string' && result.id)
 })
