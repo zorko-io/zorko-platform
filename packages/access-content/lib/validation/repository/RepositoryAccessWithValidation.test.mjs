121ded2e25bcf008619a7ba5096eb14320bb7dc3
Igor Nesterenko <igor.nesterenko@changehealthcare.com>
2021-04-22 10:53:44 +0300

    gh-242 Provide spaces access module - adding input validation layer for repository access

---
+++ packages/access-content/lib/validation/repository/RepositoryAccessWithValidation.test.mjs
@@ -0,0 +1,130 @@
+import test from '@zorko-io/tool-test-harness'
+import {RepositoryAccessWithValidation} from './RepositoryAccessWithValidation'
+import {RepositoryAccess} from '../../core'
+import sinon from 'sinon'
+import {ResourceAccessError} from '@zorko-io/util-error'
+
+test.beforeEach((t) => {
+  const sandbox = sinon.createSandbox()
+  const origin = sandbox.createStubInstance(RepositoryAccess)
+  const returnResult = 'some-result'
+
+  origin.add.returns(returnResult)
+  origin.get.returns(returnResult)
+  origin.remove.returns(returnResult)
+  origin.iterate.returns(returnResult)
+
+  const repository = new RepositoryAccessWithValidation({
+    origin
+  })
+
+  t.context = {
+    repository,
+    origin,
+    returnResult,
+    sandbox
+  }
+})
+
+test.afterEach((t)=>{
+  t.context.sandbox.restore()
+})
+
+test('add - check required params', async (t) => {
+  const { repository } = t.context
+
+  await t.throwsAsync( async () => {
+    await repository.add({})
+  }, {
+    instanceOf: ResourceAccessError,
+    message:  'ValidationError: {"content":"REQUIRED","resource":"REQUIRED","repository":"REQUIRED"}'
+  })
+
+  await t.throwsAsync( async () => {
+    await repository.add({
+      content: {},
+      resource: {},
+      repository: {}
+    })
+  }, {
+    instanceOf: ResourceAccessError,
+    message:   'ValidationError: {"resource":{"dir":"REQUIRED","name":"REQUIRED","mime":"REQUIRED","permission":"REQUIRED"},"repository":{"name":"REQUIRED","owner":"REQUIRED"}}',
+  })
+})
+
+test('add - check proper formats', async (t) => {
+  const { repository } = t.context
+
+  await t.throwsAsync( async () => {
+    await repository.add({
+      content: 'fdfdfdf',
+      resource: 'dddddd',
+      repository: 'dfdfdffd'
+    })
+  }, {
+    instanceOf: ResourceAccessError,
+    message:  'ValidationError: {"content":"FORMAT_ERROR","resource":"FORMAT_ERROR","repository":"FORMAT_ERROR"}',
+  })
+
+  await t.throwsAsync( async () => {
+    await repository.add({
+      content: 1111,
+      resource: {
+        dir: {},
+        mime: {},
+        name: [],
+        permission: [],
+        preview: {}
+      },
+      repository: {
+        name: [],
+        owner: {},
+      }
+    })
+  }, {
+    instanceOf: ResourceAccessError,
+    message:  'ValidationError: {"content":"FORMAT_ERROR","resource":{"dir":"FORMAT_ERROR","name":"FORMAT_ERROR","mime":"FORMAT_ERROR","preview":"FORMAT_ERROR","permission":"FORMAT_ERROR"},"repository":{"name":"FORMAT_ERROR","owner":"FORMAT_ERROR"}}'
+  })
+})
+
+test('get - check required and format params', async (t) => {
+  const { repository } = t.context
+
+  await t.throwsAsync( async () => {
+    await repository.get({})
+  }, {
+    instanceOf: ResourceAccessError,
+    message: 'ValidationError: {"name":"REQUIRED","owner":"REQUIRED"}',
+  })
+
+  await t.throwsAsync( async () => {
+    await repository.get({
+      name: {},
+      owner: {}
+    })
+  }, {
+    instanceOf: ResourceAccessError,
+    message: 'ValidationError: {"name":"FORMAT_ERROR","owner":"FORMAT_ERROR"}',
+  })
+})
+
+test('remove - check required and format params', async (t) => {
+  const { repository } = t.context
+
+  await t.throwsAsync( async () => {
+    await repository.remove({})
+  }, {
+    instanceOf: ResourceAccessError,
+    message: 'ValidationError: {"name":"REQUIRED","owner":"REQUIRED"}'
+  })
+
+  await t.throwsAsync( async () => {
+    await repository.remove({
+      name: {},
+      owner: {}
+    })
+  }, {
+    instanceOf: ResourceAccessError,
+    message: 'ValidationError: {"name":"FORMAT_ERROR","owner":"FORMAT_ERROR"}',
+  })
+})
