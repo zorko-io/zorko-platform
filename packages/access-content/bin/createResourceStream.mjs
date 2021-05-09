99e6cd4c889bc7cc088f362a1d45a6b52f31f1ad
Igor Nesterenko <igor.nesterenko@changehealthcare.com>
2021-05-09 11:45:39 +0300

    gh-242 Provide spaces access module  - extract resource stream

---
+++ packages/access-content/bin/createResourceStream.mjs
@@ -0,0 +1,67 @@
+import {stringify} from 'ndjson'
+import {readdir, readFile} from 'fs/promises'
+import {basename, join} from 'path'
+import {MimeTypes} from '../lib'
+
+export async function createResourceStream( {path}) {
+
+  const stream = stringify()
+  const repoDir = '/'
+
+  try {
+    const files = await readdir(path)
+    const specs  = findVegaLiteSpec(files)
+    const previews = createPreviewsMap(files)
+
+    for (let spec of specs) {
+      let name = basename(spec, '.vl.json')
+      let preview = previews[name]
+      spec = await readFile(join(path, spec), {
+        encoding: 'utf-8'
+      })
+      spec = JSON.parse(spec)
+
+      preview = await readFile(join(path, previews[name]), {
+        encoding: 'base64'
+      })
+
+      stream.write({
+        resource : {
+          name,
+          mime: MimeTypes.VegaLite,
+          preview: toDataUri(preview)
+        },
+        content: spec,
+        folder: {
+          path: repoDir + name
+        }
+      })
+    }
+
+  } catch (err) {
+    console.error(err);
+  }
+
+  return stream
+}
+
+
+function createPreviewsMap(files) {
+  return files
+    .reduce((memo, file) => {
+      if (file.indexOf('vl.png') !== -1) {
+        const name = basename(file, '.vl.png')
+        memo[name] = file
+      }
+
+      return memo
+    } , {})
+}
+
+function findVegaLiteSpec(files) {
+  return files.filter(name => name.indexOf('vl.json') !== -1)
+}
+
+function toDataUri(content) {
+  return `data:image/png;base64,${content}`
+}
--- packages/access-content/bin/readProject.mjs
+++ packages/access-content/bin/readProject.mjs
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
 
-import {readdir, readFile} from 'fs/promises'
-import {join, basename} from 'path'
-import {stringify} from 'ndjson'
-import {MimeTypes} from '../lib'
+import {createResourceStream} from './createResourceStream'
 const {argv, stdout} = process
 
 main({
@@ -11,63 +8,8 @@ main({
 }).then(console.log)
 
 async function main({path} = {}){
-  const stream = stringify()
+  let stream =  await createResourceStream({
+    path
+  })
   stream.pipe(stdout)
-  const repoDir = '/'
-
-  try {
-    const files = await readdir(path)
-    const specs  = findVegaLiteSpec(files)
-    const previews = createPreviewsMap(files)
-
-    for (let spec of specs) {
-      let name = basename(spec, '.vl.json')
-      let preview = previews[name]
-      spec = await readFile(join(path, spec), {
-        encoding: 'utf-8'
-      })
-      spec = JSON.parse(spec)
-
-      preview = await readFile(join(path, previews[name]), {
-        encoding: 'base64'
-      })
-
-      stream.write({
-        resource : {
-          name,
-          mime: MimeTypes.VegaLite,
-          preview: toDataUri(preview)
-        },
-        content: spec,
-        folder: {
-          path: repoDir + name
-        }
-      })
-    }
-
-  } catch (err) {
-    console.error(err);
-  } finally {
-    stream.end()
-  }
-}
-
-function createPreviewsMap(files) {
-  return files
-    .reduce((memo, file) => {
-      if (file.indexOf('vl.png') !== -1) {
-        const name = basename(file, '.vl.png')
-        memo[name] = file
-      }
-
-      return memo
-    } , {})
-}
-
-function findVegaLiteSpec(files) {
-  return files.filter(name => name.indexOf('vl.json') !== -1)
-}
-
-function toDataUri(content) {
-  return `data:image/png;base64,${content}`
 }
