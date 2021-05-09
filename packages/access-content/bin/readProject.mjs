762a2a5fb6dd4b5b7a89d19846fe856663541500
Igor Nesterenko <igor.nesterenko@changehealthcare.com>
2021-05-09 11:27:29 +0300

    gh-242 Provide spaces access module  - merge preview and spec

--- packages/access-content/bin/readProject.mjs
+++ packages/access-content/bin/readProject.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import {readdir, readFile} from 'fs/promises'
-import {join, resolve} from 'path'
+import {join, basename} from 'path'
 import {stringify} from 'ndjson'
 import {MimeTypes} from '../lib'
 const {argv, stdout} = process
@@ -16,34 +16,33 @@ async function main({path} = {}){
   const repoDir = '/'
 
   try {
+    const files = await readdir(path)
+    const specs  = findVegaLiteSpec(files)
+    const previews = createPreviewsMap(files)
 
-    const files = await readdir(path);
-    for (const file of files) {
-      let split = file.split('.')
-      const name = split[0]
-      const extension = split.pop()
-      if (extension === 'json') {
-        let spec = await readFile(join(path, file), {
-          encoding: 'utf-8'
-        })
-        spec = JSON.parse(spec)
-        stream.write({
-          resource : {
-            name,
-            mime: MimeTypes.VegaLite
-          },
-          content: spec,
-          folder: {
-            path: repoDir + name
-          }
-        })
-      }else if (extension === 'png') {
-        // let preview = await readFile(join(path, file), {encoding: 'base64'})
-        // stream.write({
-        //   name,
-        //   preview: toDataUri(preview)
-        // })
-      }
+    for (let spec of specs) {
+      let name = basename(spec, '.vl.json')
+      let preview = previews[name]
+      spec = await readFile(join(path, spec), {
+        encoding: 'utf-8'
+      })
+      spec = JSON.parse(spec)
+
+      preview = await readFile(join(path, previews[name]), {
+        encoding: 'base64'
+      })
+
+      stream.write({
+        resource : {
+          name,
+          mime: MimeTypes.VegaLite,
+          preview: toDataUri(preview)
+        },
+        content: spec,
+        folder: {
+          path: repoDir + name
+        }
+      })
     }
 
   } catch (err) {
@@ -51,6 +50,22 @@ async function main({path} = {}){
   } finally {
     stream.end()
   }
+}
+
+function createPreviewsMap(files) {
+  return files
+    .reduce((memo, file) => {
+      if (file.indexOf('vl.png') !== -1) {
+        const name = basename(file, '.vl.png')
+        memo[name] = file
+      }
+
+      return memo
+    } , {})
+}
+
+function findVegaLiteSpec(files) {
+  return files.filter(name => name.indexOf('vl.json') !== -1)
 }
 
 function toDataUri(content) {
